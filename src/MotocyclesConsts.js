4ab0341cb7d20789c170b05a9aef6013f2f778d2
Yuizhey <Gre4ka28@yandex.ru>
2025-03-18 23:23:02 +0300

    обновил фотки структуру

---
+++ src/MotocyclesConsts.js
@@ -0,0 +1,52 @@
+let motocyclesConsts = [
+    {
+      imagePath: "public/images/CBR.png",
+      name: "CBR"
+    },
+    {
+        imagePath: "public/images/Honda 140.png",
+        name: "Honda 140"
+    },
+    {
+        imagePath: "public/images/KPR 150.png",
+        name: "KPR 150"
+    },
+    {
+        imagePath: "public/images/Kawasaki 360.png",
+        name: "Kawasaki 360"
+    },
+    {
+        imagePath: "public/images/Gixser.png",
+        name: "Gixser"
+    },
+    {
+        imagePath: "public/images/moto.png",
+        name: "MT15"
+    },
+    {
+        imagePath: "public/images/CVR.png",
+        name: "CVR"
+    },
+    {
+        imagePath: "public/images/R1 5.png",
+        name: "R1 5"
+    },
+    {
+        imagePath: "public/images/Kawasaki !40.png",
+        name: "Kawasaki !40"
+    },
+    {
+        imagePath: "public/images/MT15.png",
+        name: "MT15"
+    },
+    {
+        imagePath: "public/images/Filder 350.png",
+        name: "Filder 350"
+    },
+    {
+        imagePath: "public/images/Scookiti.png",
+        name: "Scookiti"
+    },
+  ];
+
+  export default motocyclesConsts;
--- src/components/MotoCardsGrid/MotoCardsGrid.jsx
+++ src/components/MotoCardsGrid/MotoCardsGrid.jsx
@@ -1,22 +1,15 @@
 import React from 'react'
 import MotoCard from '../MotoCard/MotoCard'
 import styles from "./MotoCardsGrid.module.css"
+import motocyclesConsts from "../../MotocyclesConsts"
 
 function MotoCardsGrid() {
+  
   return (
     <div className={styles.motoCardsGrid}>
-        <MotoCard imagePath="public/images/moto.png" name="CBR" />
-        <MotoCard imagePath="public/images/moto.png" name="CBR" />
-        <MotoCard imagePath="public/images/moto.png" name="CBR" />
-        <MotoCard imagePath="public/images/moto.png" name="CBR" />
-        <MotoCard imagePath="public/images/moto.png" name="CBR" />
-        <MotoCard imagePath="public/images/moto.png" name="CBR" />
-        <MotoCard imagePath="public/images/moto.png" name="CBR" />
-        <MotoCard imagePath="public/images/moto.png" name="CBR" />
-        <MotoCard imagePath="public/images/moto.png" name="CBR" />
-        <MotoCard imagePath="public/images/moto.png" name="CBR" />
-        <MotoCard imagePath="public/images/moto.png" name="CBR" />
-        <MotoCard imagePath="public/images/moto.png" name="CBR" />
+        {motocyclesConsts.map(item => (
+          <MotoCard imagePath={item.imagePath} name={item.name} />
+        ))}
     </div>
   )
 }
