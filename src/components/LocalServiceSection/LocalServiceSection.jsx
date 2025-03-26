279ea45baa8b9617dbe6af8df4ca0a9789688787
Yuizhey <Gre4ka28@yandex.ru>
2025-03-26 10:22:47 +0300

    почти доделал слайдер на странице presentation

--- src/components/LocalServiceSection/LocalServiceSection.jsx
+++ src/components/LocalServiceSection/LocalServiceSection.jsx
@@ -1,20 +1,60 @@
-import React from 'react'
-import { GrPrevious } from "react-icons/gr";
-import { GrNext } from "react-icons/gr";
-import images from "../../constants/LocalServiceSectionImages.js"
-import styles from "./LocalServiceSection.module.css"
+import React, { useState } from 'react';
+import { GrPrevious, GrNext } from "react-icons/gr";
+import images from "../../constants/LocalServiceSectionImages.js";
+import styles from "./LocalServiceSection.module.css";
 
 function LocalServiceSection() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const nextSlide = () => {
+    setCurrentIndex((prevIndex) => 
+      prevIndex === images.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+
+  const prevSlide = () => {
+    setCurrentIndex((prevIndex) => 
+      prevIndex === 0 ? images.length - 1 : prevIndex - 1
+    );
+  };
+
+  const getVisibleImages = () => {
+    const visibleImages = [];
+    for (let i = 0; i < 5; i++) {
+      const index = (currentIndex + i) % images.length;
+      visibleImages.push(images[index]);
+    }
+    return visibleImages;
+  };
+
   return (
     <div className={styles.container}>
-        <h1 className={styles["container-title"]}>Local Service We Provide</h1>
-        <div className={styles["container-images"]}>
-            <button className="button"><GrPrevious /></button>
-            {images.slice(-5).map(item=> <img src={item.path} className={styles.image}/>)}
-            <button className="button"><GrNext /></button>
+      <h1 className={styles.title}>Local Service We Provide</h1>
+      
+      <div className={styles.slider}>
+        <button className={styles.navButton} onClick={prevSlide}>
+          <GrPrevious />
+        </button>
+        
+        <div className={styles.sliderTrack}>
+          {getVisibleImages().map((item) => (
+            <div key={item.id} className={styles.slideContainer}>
+              <img 
+                src={item.path} 
+                className={styles.slideImage}
+                alt={`Service ${item.id}`}
+              />
+              <p className={styles.locationText}>{item.text}</p>
+            </div>
+          ))}
         </div>
+        
+        <button className={styles.navButton} onClick={nextSlide}>
+          <GrNext />
+        </button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default LocalServiceSection
+export default LocalServiceSection;
--- src/constants/LocalServiceSectionImages.js
+++ src/constants/LocalServiceSectionImages.js
@@ -1,31 +1,38 @@
 const images = [
     {
         id: 1,
-        path: "images/car5.png"
+        path: "images/car5.png",
+        text: "Gazipur"
     },
     {
         id: 2,
-        path: "images/CBR.png"
+        path: "images/CBR.png",
+        text: "Mohammad Pur"
     },
     {
         id: 3,
-        path: "images/car5.png"
+        path: "images/car5.png",
+        text: "Gulshan"
     },
     {
         id: 4,
-        path: "images/CBR.png"
+        path: "images/CBR.png",
+        text: "Dhammondi"
     },
     {
         id: 5,
-        path: "images/car5.png"
+        path: "images/car5.png",
+        text: "Mohakali"
     },
     {
         id: 6,
-        path: "images/CBR.png"
+        path: "images/CBR.png",
+        text: "text"
     },
     {
         id: 7,
-        path: "images/car5.png"
+        path: "images/car5.png",
+        text: "text"
     },
 ];
 
